19a4896f5c1ae0bd43b879efec771a94dfd52b5d
zd2c12 <zd2c12@gmail.com>
2018-01-04 22:43:36 +0900

    home0104

--- html/js/word.js
+++ html/js/word.js
@@ -1,66 +1,33 @@
 
     // ナブメニュー文字変換
     $(function() {
-        $('.navbar-order a').hover(function() {
+        $('.navbar-order a, .main-order a').hover(function() {
             $(this).text('洋服のオーダーメイド');
         }, function() {
             $(this).text('Order');
         });
-        $('.navbar-remake a').hover(function() {
+        $('.navbar-remake a, .main-remake a').hover(function() {
             $(this).text('洋服・衣装のリメイク');
         }, function() {
             $(this).text('Remake');
         });
-        $('.navbar-coordination a').hover(function() {
+        $('.navbar-coordination a, .main-coordination a').hover(function() {
             $(this).text('衣装のコーディネイト');
         }, function() {
             $(this).text('Coordination');
         });
-        $('.navbar-story a').hover(function() {
+        $('.navbar-story a, .main-story a').hover(function() {
             $(this).text('Mijose物語');
         }, function() {
             $(this).text('Mijose Story');
         });
-        $('.navbar-profile a').hover(function() {
+        $('.navbar-profile a, .main-profile a').hover(function() {
             $(this).text('プロフィール');
         }, function() {
             $(this).text('Profile');
         });
 
-        $('.navbar-mail a').hover(function() {
-            $(this).text('メール');
-        }, function() {
-            $(this).text('Mail');
-        });
-    });
-    // メイン文字変換
-    $(function() {
-        $('.main-order a').hover(function() {
-            $(this).text('洋服のオーダーメイド');
-        }, function() {
-            $(this).text('Order');
-        });
-        $('.main-remake a').hover(function() {
-            $(this).text('洋服・衣装のリメイク');
-        }, function() {
-            $(this).text('Remake');
-        });
-        $('.main-coordination a').hover(function() {
-            $(this).text('衣装のコーディネイト');
-        }, function() {
-            $(this).text('Coordination');
-        });
-        $('.main-story a').hover(function() {
-            $(this).text('Mijose物語');
-        }, function() {
-            $(this).text('Mijose Story');
-        });
-        $('.main-profile a').hover(function() {
-            $(this).text('プロフィール');
-        }, function() {
-            $(this).text('Profile');
-        });
-        $('.main-mail a').hover(function() {
+        $('.navbar-mail a, .main-mail a').hover(function() {
             $(this).text('メール');
         }, function() {
             $(this).text('Mail');
