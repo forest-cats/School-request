59a767d5b29489403b95082f55962a00f6db2f0f
SIM\zd2C12 <snake.snake.poison.snake@gmail.com>
2018-01-10 15:59:32 +0900

    school0110-2

--- html/js/accordion.js
+++ html/js/accordion.js
@@ -11,35 +11,35 @@
     $(function() {
         $('#accordion dd').hide();
         $('.navbar-order a').click(function() {
-            $('#accordion .main-order-dd').slideToggle();
-            $('#accordion .main-order-dd').siblings('dd').slideUp();
+            $('#accordion .dd-order').slideToggle();
+            $('#accordion .dd-order').siblings('dd').slideUp();
         });
     });
     $(function() {
         $('#accordion dd').hide();
         $('.navbar-remake a').click(function() {
-            $('#accordion .main-remake-dd').slideToggle();
-            $('#accordion .main-remake-dd').siblings('dd').slideUp();
+            $('#accordion .dd-remake').slideToggle();
+            $('#accordion .dd-remake').siblings('dd').slideUp();
         });
     });
     $(function() {
         $('#accordion dd').hide();
         $('.navbar-coordination a').click(function() {
-            $('#accordion .main-coordination-dd').slideToggle();
-            $('#accordion .main-coordination-dd').siblings('dd').slideUp();
+            $('#accordion .dd-coordination').slideToggle();
+            $('#accordion .dd-coordination').siblings('dd').slideUp();
         });
     });
     $(function() {
         $('#accordion dd').hide();
         $('.navbar-story a').click(function() {
-            $('#accordion .main-story-dd').slideToggle();
-            $('#accordion .main-story-dd').siblings('dd').slideUp();
+            $('#accordion .dd-story').slideToggle();
+            $('#accordion .dd-story').siblings('dd').slideUp();
         });
     });
     $(function() {
         $('#accordion dd').hide();
         $('.navbar-profile a').click(function() {
-            $('#accordion .main-profile-dd').slideToggle();
-            $('#accordion .main-profile-dd').siblings('dd').slideUp();
+            $('#accordion .dd-profile').slideToggle();
+            $('#accordion .dd-profile').siblings('dd').slideUp();
         });
     });
