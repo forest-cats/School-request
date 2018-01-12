1e7fcb5376c60ab2032ec5a47d41f770f21a709d
SIM\zd2C12 <snake.snake.poison.snake@gmail.com>
2018-01-12 11:00:02 +0900

    school-0112-OK

--- html/js/accordion.js
+++ html/js/accordion.js
@@ -1,45 +1,61 @@
+// アコーディオン(メイン&めーる)
+$(function() {
+    $('#accordion dd').hide();
+    $('#accordion dt').click(function() {
+        $(this).next('dd').slideToggle();
+        $(this).next('dd').siblings('dd').slideUp();
+    });
+});
+// $(function() {
+//     $('#mail-accordion dd').hide();
+//     $('#mail-accordion dt').click(function() {
+//         $(this).next('dd').slideToggle();
+//         $(this).next('dd').siblings('dd').slideUp();
+//     });
+// });
 
-    // アコーディオン(メイン)
-    $(function() {
-        $('#accordion dd').hide();
-        $('#accordion dt').click(function() {
-            $(this).next('dd').slideToggle();
-            $(this).next('dd').siblings('dd').slideUp();
-        });
+
+// アコーディオン(ナビゲーション用)
+$(function() {
+    $('#accordion dd').hide();
+    $('.navbar-order a').click(function() {
+        $('#accordion .dd-order').slideToggle();
+        $('#accordion .dd-order').siblings('dd').slideUp();
     });
-    // アコーディオン(ナビゲーション用)
-    $(function() {
-        $('#accordion dd').hide();
-        $('.navbar-order a').click(function() {
-            $('#accordion .dd-order').slideToggle();
-            $('#accordion .dd-order').siblings('dd').slideUp();
-        });
+});
+$(function() {
+    $('#accordion dd').hide();
+    $('.navbar-remake a').click(function() {
+        $('#accordion .dd-remake').slideToggle();
+        $('#accordion .dd-remake').siblings('dd').slideUp();
     });
-    $(function() {
-        $('#accordion dd').hide();
-        $('.navbar-remake a').click(function() {
-            $('#accordion .dd-remake').slideToggle();
-            $('#accordion .dd-remake').siblings('dd').slideUp();
-        });
+});
+$(function() {
+    $('#accordion dd').hide();
+    $('.navbar-coordination a').click(function() {
+        $('#accordion .dd-coordination').slideToggle();
+        $('#accordion .dd-coordination').siblings('dd').slideUp();
     });
-    $(function() {
-        $('#accordion dd').hide();
-        $('.navbar-coordination a').click(function() {
-            $('#accordion .dd-coordination').slideToggle();
-            $('#accordion .dd-coordination').siblings('dd').slideUp();
-        });
+});
+$(function() {
+    $('#accordion dd').hide();
+    $('.navbar-story a').click(function() {
+        $('#accordion .dd-story').slideToggle();
+        $('#accordion .dd-story').siblings('dd').slideUp();
     });
-    $(function() {
-        $('#accordion dd').hide();
-        $('.navbar-story a').click(function() {
-            $('#accordion .dd-story').slideToggle();
-            $('#accordion .dd-story').siblings('dd').slideUp();
-        });
+});
+$(function() {
+    $('#accordion dd').hide();
+    $('.navbar-profile a').click(function() {
+        $('#accordion .dd-profile').slideToggle();
+        $('#accordion .dd-profile').siblings('dd').slideUp();
     });
-    $(function() {
-        $('#accordion dd').hide();
-        $('.navbar-profile a').click(function() {
-            $('#accordion .dd-profile').slideToggle();
-            $('#accordion .dd-profile').siblings('dd').slideUp();
-        });
-    });
+});
+
+// $(function() {
+//     $('#mail-accordion dd').hide();
+//     $('.navbar-mail a').click(function() {
+//         $('#mail-accordion .dd-mail').slideToggle();
+//         $('#mail-accordion .dd-mail').siblings('dd').slideUp();
+//     });
+// });
